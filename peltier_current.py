5dd654418eb8ebdc788ed0987595b54e707adc42
manpreet833 <43992850+manpreet833@users.noreply.github.com>
2018-11-21 15:50:32 +0100

    Update peltier_current.py

--- peltier_current.py
+++ peltier_current.py
@@ -1,20 +1,11 @@
 import time, serial, math
-
-# def sensor_read(number):
-#     bus=smbus.SMBus(number)
-#     bus.write_i2c_block_data(0x44, 0x2c, [0x06])
-#     time.sleep(0.5)
-#     #SHT31 adress, 0x44(68)
-#     #Read data back from 0x00(00), 6 bytes
-#     #Temp MSB, temp LSB, Temp CRC, Humidity MSB, Humidity LSB , Humidity CRC
-#     data = bus.read_i2c_block_data(0x44, 0x00, 6)
-#     #Convert the data
-#     temp=data[0] * 256 + data[1]
-#     cTemp = -45 + (175*temp/65535.0)
-#     return cTemp
-
-# Th = sensor_read(1)
-# Tc = sensor_read(2)
+import glob
+import time
+from time import sleep
+import RPi.GPIO as GPIO
+sleeptime = 1
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0,
             bytesize=serial.EIGHTBITS,
@@ -22,65 +13,103 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0,
             stopbits=serial.STOPBITS_ONE)
 
 x  = 1
+print 'Initialising System...'
+
+
+def TemperatureRead(device_folder):
+    base_dir = '/sys/bus/w1/devices/28-00000'
+    device_file = base_dir + device_folder + '/w1_slave'
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = TemperaturMessung()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+# 0 means top
+
+sensor_id1 = 'a294445'
+sensor_id2 = 'a2a4342'
+sensor_id3 = 'a2a8f7c'
+sensor_id4 = 'a2a8c45'
+sensor_id5 = 'a2ab77b'
+sensor_id6 = 'a291dd5'
+
+def update_current():
+    Th[0] = TemperatureRead(sensor_id1)
+    Th[1] = TemperatureRead(sensor_id2)
+    Th[2] = TemperatureRead(sensor_id3)
 
 
+    Tc[0] = TemperatureRead(sensor_id4)
+    Tc[1] = TemperatureRead(sensor_id5)
+    Tc[2] = TemperatureRead(sensor_id6)
+
+    TH = (Th[0] + Th[1] + Th[2])/3
+    TC = (Tc[0] + Tc[1] + Tc[2])/3
+    # Th = 35.00#float(input('Hot side temp'))
+    # Tc = 15.00#float(input('Cold side temp'))
 
 
-Th = 35.00#float(input('Hot side temp'))
-Tc = 15.00#float(input('Cold side temp'))
+    deltaT = TH - TC 
+    k = 1.394117647             #thermal conductance (W/K)
+    Qc = 40#float(input('Cooling power'))                        #cooling power(W)
+    R = 0.79946028             #electrical resistance(Ohms)
+    alpha =0.051298701         #Seebecks coeff(V/K)
+    b = (-2* alpha*Tc)/R
+    c = (-Qc +k*deltaT)*2/R
+    I1 = float((-b+math.sqrt(b**2 -4*c ))/2)
+    # I2 = (-b-math.sqrt(b**2 -4*c ))/2
+     
+    #Qc = -(alpha*I*Tc) + (0.5*(I**2)*R) + k*(Th-Tc)
+    print (I1)
+    if len(str(int(I1))) == 1:
+        current_str = 'CURR0' + str(int(I1*10)) + '\r'
+    if len(str(int(I1))) ==2:
+        current_str = 'CURR' + str(int(I1*10)) + '\r'
+
+        
+
+    if ser.isOpen():  #exit if USB port is not open
+        try:
+            ser.flushInput() #flush input buffer, discarding all its contents
+            ser.flushOutput() #flush output buffer, aborting current output
+
+            while x:        
+              ser.write("GETS\r")       # Get setting information
+              time.sleep(0.5)
+              response=ser.readline()
+              if response == '':continue
+
+              voltage=response[:3]  # PSU return string in format "100200", that means 10.0V, 2.00A,
+              current=response[3:6] # then use first 3 data for voltage and next 3 data for current.
+              print(voltage[:2]+'.'+voltage[2:]+'V')
+              print(current[:1]+'.'+current[1:]+'A')    
+                      break
+            time.sleep(3)
 
 
-deltaT = Th - Tc 
-k = 1.394117647             #thermal conductance (W/K)
-Qc = 40#float(input('Cooling power'))                        #cooling power(W)
-R = 0.79946028             #electrical resistance(Ohms)
-alpha =0.051298701         #Seebecks coeff(V/K)
-b = (-2* alpha*Tc)/R
-c = (-Qc +k*deltaT)*2/R
-I1 = (-b+math.sqrt(b**2 -4*c ))/2
-# I2 = (-b-math.sqrt(b**2 -4*c ))/2
- 
-#Qc = -(alpha*I*Tc) + (0.5*(I**2)*R) + k*(Th-Tc)
-print (I1)
-current_str = 'CURR' + str(I1*100) + '\r'
+            #Set PSU output to 2A
+            print("Set PSU CURRENT OUTPUT to ") + str(I1)
+            while x:
+              ser.write(current_str)  # change Current setting
+              time.sleep(0.5)
+              response=ser.readline()
+              if response == '':continue            
+              print(response)
+              break
+            time.sleep(3)
+
+            ser.close()
+
+        except Exception, e1:
+            print "Error communicating...: " + str(e1)
 
 
-
-if ser.isOpen():  #exit if USB port is not open
-    try:
-        ser.flushInput() #flush input buffer, discarding all its contents
-        ser.flushOutput() #flush output buffer, aborting current output
-
-        while x:        
-          ser.write("GETS\r")       # Get setting information
-          time.sleep(0.5)
-          response=ser.readline()
-          if response == '':continue
-
-          voltage=response[:3]  # PSU return string in format "100200", that means 10.0V, 2.00A,
-          current=response[3:6] # then use first 3 data for voltage and next 3 data for current.
-          print(voltage[:2]+'.'+voltage[2:]+'V')
-          print(current[:1]+'.'+current[1:]+'A')    
-                  break
-        time.sleep(3)
-
-
-        #Set PSU output to 2A
-        print("Set PSU CURRENT OUTPUT to ") + str(I1)
-        while x:
-          ser.write(current_str)  # change Current setting
-          time.sleep(0.5)
-          response=ser.readline()
-          if response == '':continue            
-          print(response)
-          break
-        time.sleep(3)
-
-        ser.close()
-
-    except Exception, e1:
-        print "Error communicating...: " + str(e1)
-
-
-else:
-    print "Cannot open serial port "
+    else:
+        print "Cannot open serial port "
