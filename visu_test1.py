f1c009d93622061e3c6e1583eb48103fd34eefb0
atul0058 <44167761+atul0058@users.noreply.github.com>
2018-11-22 12:18:58 +0100

    Update visu_test1.py

--- visu_test1.py
+++ visu_test1.py
@@ -7,11 +7,24 @@ for i in range(12):
     	label[i][j] = Label(my_window, width = '10', height = '3', bg = 'blue')
 
 
-img1=PhotoImage(file="rack.png")
-rack=[]
-for i in range(4):
-    rack.append('Label(my_window,image=img1)')
 
+def putwkp(i,j):
+    nw_rows=[1,3,5,7,9,11]
+    other_rows=[0,2,4,6,8,10]
+    if i in nw_rows:
+        label[i][j].grid(row=i,column=j,sticky="nw")
+    elif i==0 and j==0:
+        label[0][0].grid(row=0,column=0,sticky="e")
+    elif i in other_rows:
+        label[i][j].grid(row=i,column=j)
+
+def put_img(i,j,img):
+    label[i][j]= Label(my_window,image=img)
+    putwkp(i,j)
+    label[i][j].photo = img
+        
+
+#think anout it. put_wkp should replace this
 nw_rows=[1,3,5,7,9,11]
 for i in nw_rows:
     for j in range(4):
@@ -21,6 +34,7 @@ other_rows=[0,2,4,6,8,10]
 for i in other_rows:
 	for j in range(4):
 		label[i][j].grid(row=i,column=j)
+
 
 #exception
 label[0][0].grid(row=0,column=0,sticky="e")
@@ -39,6 +53,7 @@ def clearstore():#add modbus
             print("The store has been cleared")
 
 #putting racks
+img1=PhotoImage(file="rack.png")
 label_1=Label(my_window,image=img1)
 label_2=Label(my_window,image=img1)
 label_3=Label(my_window,image=img1)
@@ -55,20 +70,45 @@ label_6.grid(row=10,column=0,columnspan=4, rowspan=2)
 
 #input is location and color, will put the wkp in the locaiton
 def show_image(location,color):
+    #location=input("Enter location: ")#replace with SQL query
+    #color=input("Enter color: ")#replace with SQL query
+    location=int(location)
+    shelf=location//100
+    row=(location%100)//10
+    i=12-2*shelf
+    j=row-1
     if color=="red":
-        img=PhotoImage(file="Dose_schwarz.png")
-        #add location 
+        img=PhotoImage(file="Dose_rot.png")
+        put_img(i,j,img)
+        #my_window.after(1000,show_image)
     if color=="silver":
         img=PhotoImage(file="Dose_silber.png")
-    if color=="black_wkp":
+        put_img(i,j,img)
+        #my_window.after(1000,show_image)
+    if color=="black":
         img=PhotoImage(file="Dose_schwarz,png")
-    if color=="trans_wkp":
+        put_img(i,j,img)
+        #my_window.after(1000,show_image)
+    if color=="transparent":
         img=PhotoImage(file="Dose_transparent.png")
+        put_img(i,j,img)
+        #my_window.after(1000,show_image)
 
-def remove_wkp(location):
-    #make locaiton as i and j
+def remove_wkp():
+    location=input("Enter location: ")#replace with SQL query
+    location=int(location)
+    shelf=location//100
+    row=(location%100)//10
+    i=12-2*shelf
+    j=row-1
     label[i][j].grid_forget()
 
+
+show_image(611,"red")
+
+#my_window.after(0,show_image)
+
+#my_window.mainloop() 
 
 
 
